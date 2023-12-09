0000efa7c6be892b180d44b901a72a832f09f118
Otávio Silva <otaviosilva2632@gmail.com>
2023-12-09 12:00:08 -0300

    feat: learning basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // components, directives, pipes (created by you and for other libs like angular material)
+    AppComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [], // services (declaring services here, you can use dependency injection in constructor of components)
+  exports: [], // all the things you want export to other modules
+  bootstrap: [AppComponent], // which component will be rendered in your app
 })
-export class AppModule { }
+export class AppModule {}
