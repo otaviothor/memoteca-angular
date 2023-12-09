532e1d37314cffc29dc0e56844c9b097842364bd
Otávio Silva <otaviosilva2632@gmail.com>
2023-12-09 12:02:52 -0300

    feat: create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
     // components, directives, pipes (created by you and for other libs like angular material)
     AppComponent,
     HomeComponent,
+    CabecalhoComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [], // services (declaring services here, you can use dependency injection in constructor of components)
